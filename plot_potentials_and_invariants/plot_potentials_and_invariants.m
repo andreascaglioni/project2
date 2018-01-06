3007cd3246fff677fc88a5b74fa0b2a33c95d6dd
scaglioni <andrea.scaglioni@epfl.ch>
2018-01-06 13:57:27 +0100

    3-well and and fast-slow

--- plot_potentials_and_invariants/plot_potentials_and_invariants.m
+++ plot_potentials_and_invariants/plot_potentials_and_invariants.m
@@ -1,18 +1,21 @@
 clc;
 close all;
-clear all;
+clear;
 %% plotting potentials and invariants
-
-potNumber = 1;
+potNumber = 3;
 switch potNumber
     case 0
         S = [1,0;0,0.5];
         pot = @(x,y) ([x,y]*S*[x;y]);
     case 1
         pot = @(x,y) (0.25*(x.^2-1).^2+0.5*y.^2);
+    case 2
+        pot = @(x,y) (0.25*(y.^2-1).^2+ 1.25*(y-0.5*x).^2);
+    case 3 
+        pot = @(x,y) (3*exp(-x.^2-(y-1/3).^2)-3*exp(-x.^2-(y-5/3).^2)-5*exp(-(x-1).^2-y.^2)-5*exp(-(x+1).^2-y.^2)+0.2*x.^4+0.2*(y-1/3)^4);
 end
 inv = @(x,y) (exp(-pot(x,y)));
-xx = -2.5:0.1:2.5;
+xx = -3:0.1:3;
 inv_plot = zeros(length(xx));
 pot_plot = inv_plot;
 for i = 1:length(xx)
@@ -22,16 +25,17 @@ for i = 1:length(xx)
         
     end
 end
-
+%% plot
 figure;
 colormap(parula);
 subplot(1,2,1)
 contour(xx,xx,inv_plot,10);
+[C,h] = contour(xx,xx,pot_plot,-4:0.5:4);   % for 3 well
 grid on;
 colorbar;
 pbaspect([1 1 1]);
 subplot(1,2,2)
-[C,h] = contour(xx,xx,pot_plot,0.1:0.2:1.9);    % for bistable
+%[C,h] = contour(xx,xx,pot_plot,0.1:0.2:1.9);    % for bistable
 grid on;
 colorbar;
 pbaspect([1 1 1]);
