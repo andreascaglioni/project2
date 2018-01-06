77c16bd9f2f796912a17c1637f3bbd1ce2238e0f
scaglioni <andrea.scaglioni@epfl.ch>
2018-01-06 21:22:10 +0100

    plot 3 holw potential

--- plot_potentials_and_invariants/plot_potentials_and_invariants.m
+++ plot_potentials_and_invariants/plot_potentials_and_invariants.m
@@ -12,16 +12,17 @@ switch potNumber
     case 2
         pot = @(x,y) (0.25*(y.^2-1).^2+ 1.25*(y-0.5*x).^2);
     case 3 
-        pot = @(x,y) (3*exp(-x.^2-(y-1/3).^2)-3*exp(-x.^2-(y-5/3).^2)-5*exp(-(x-1).^2-y.^2)-5*exp(-(x+1).^2-y.^2)+0.2*x.^4+0.2*(y-1/3)^4);
+        %pot = @(x,y) (3*exp(-x.^2-(y-1/3).^2)-3*exp(-x.^2-(y-5/3).^2)-5*exp(-(x-1).^2-y.^2)-5*exp(-(x+1).^2-y.^2)+0.2*x.^4+0.2*(y-1/3)^4);
+         pot= @(x,y) 3*exp(-x.^2-(y-1/3).^2)-3*exp(-x.^2-(y-5/3).^2)-5*exp(-(x-1).^2-y.^2)-5*exp(-(x+1).^2-y.^2)+0.2*x.^4+0.2*(y-1/3).^4;
 end
 inv = @(x,y) (exp(-pot(x,y)));
-xx = -3:0.1:3;
+xx = -5:0.1:5;
 inv_plot = zeros(length(xx));
 pot_plot = inv_plot;
 for i = 1:length(xx)
     for j = 1:length(xx)
-        inv_plot(i,j) = inv(xx(i),xx(j));
-        pot_plot(i,j) = pot(xx(i), xx(j));
+        inv_plot(j,i) = inv(xx(i),xx(j));
+        pot_plot(j,i) = pot(xx(i), xx(j));
         
     end
 end
@@ -29,13 +30,16 @@ end
 figure;
 colormap(parula);
 subplot(1,2,1)
-contour(xx,xx,inv_plot,10);
-[C,h] = contour(xx,xx,pot_plot,-4:0.5:4);   % for 3 well
+contour(xx,xx,inv_plot);
+vv = -5:0.1:3; % for 3 wells
+[C,h] = contour(xx,xx,pot_plot, vv);
 grid on;
 colorbar;
 pbaspect([1 1 1]);
 subplot(1,2,2)
-%[C,h] = contour(xx,xx,pot_plot,0.1:0.2:1.9);    % for bistable
+%vv = 0.1:0.2:1.9; % for bistable
+vv = exp(-vv); % for 3 wells
+[C,h] = contour(xx,xx,inv_plot,vv);
 grid on;
 colorbar;
 pbaspect([1 1 1]);
