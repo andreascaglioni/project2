c6b5047b5dab964cd79f89b48e5b3a2f3ea9381a
scaglioni <andrea.scaglioni@epfl.ch>
2018-01-09 17:26:36 +0100

    small modifications

--- unsteady_FP/run.sh
+++ unsteady_FP/run.sh
@@ -2,13 +2,10 @@
 
 echo '# 0'
 FreeFem++ unsteady_FP.freefem 0
-echo '# 0.12'
-FreeFem++ unsteady_FP.freefem 0.12
-echo '# 0.14'
-FreeFem++ unsteady_FP.freefem 0.14
-echo '# 8'
-FreeFem++ unsteady_FP.freefem 8
-echo '# 12'
-FreeFem++ unsteady_FP.freefem 12
-echo '# 20'
-FreeFem++ unsteady_FP.freefem 20
+echo '# 10'
+FreeFem++ unsteady_FP.freefem 10
+echo '# 50'
+FreeFem++ unsteady_FP.freefem 50
+echo '# 80'
+FreeFem++ unsteady_FP.freefem 80
+
